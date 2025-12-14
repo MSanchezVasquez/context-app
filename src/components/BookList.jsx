387dd899ddca455d769b148e2addf7f61e9249d6
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-13 19:44:32 -0500

    feat: scaffold initial UI components and styles

---
+++ src/components/BookList.jsx
@@ -0,0 +1,12 @@
+export const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the way of the dragon</li>
+        <li>the art of war</li>
+        <li>the power of now</li>
+      </ul>
+    </div>
+  );
+};
+export default BookList;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My Application</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
