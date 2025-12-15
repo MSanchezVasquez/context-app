0b82e8c00617bf263392f4c8573f2f375b512c40
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-14 19:26:50 -0500

    feat: consume ThemeContext in Navbar and BookList to apply dynamic styles

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,19 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 export const BookList = () => {
+  const theme = useContext(ThemeContext);
+  const { isLightTheme, light, dark } = theme;
+  const themeCurrent = isLightTheme ? light : dark;
   return (
-    <div className="book-list">
+    <div
+      className="book-list"
+      style={{ background: themeCurrent.bg, color: themeCurrent.syntax }}
+    >
       <ul>
-        <li>the way of the dragon</li>
-        <li>the art of war</li>
-        <li>the power of now</li>
+        <li style={{ background: themeCurrent.ui }}>the way of the dragon</li>
+        <li style={{ background: themeCurrent.ui }}>the art of war</li>
+        <li style={{ background: themeCurrent.ui }}>the power of now</li>
       </ul>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 const Navbar = () => {
+  const theme = useContext(ThemeContext);
+  const { isLightTheme, light, dark } = theme;
+  const themeCurrent = isLightTheme ? light : dark;
+
   return (
-    <nav className="navbar">
+    <nav style={{ background: themeCurrent.ui, color: themeCurrent.syntax }}>
       <h1>My Application</h1>
       <ul>
         <li>Home</li>
