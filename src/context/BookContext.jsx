a69c28b62ea3c1a7ec31583ab49178ff73469590
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-17 22:36:06 -0500

    refactor: migrate BookContext to useReducer and separate logic into bookReducer

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -1,26 +1,13 @@
-import { createContext, useState } from "react";
-import { v4 as uuid } from "uuid";
+import { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 export const BookContextProvider = ({ children }) => {
-  const [books, setBooks] = useState([
-    { title: "the way of the dragon", author: "Bruce Lee", id: 1 },
-    { title: "the art of war", author: "Sun Tzu", id: 2 },
-    { title: "the power of now", author: "Eckhart Tolle", id: 3 },
-    { title: "the 48 laws of power", author: "Robert Greene", id: 4 },
-  ]);
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }]);
-  };
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
-  const totalBooks = books.length;
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook, totalBooks }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {children}
     </BookContext.Provider>
   );
